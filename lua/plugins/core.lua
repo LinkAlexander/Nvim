975329611b93dc1e77648ea5336117fb0d5a9464
Alex <alexanderlink277@gmail.com>
2025-05-23 22:58:08 +0200

    hide dotfiles. Probably doesnt work

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,8 +1,18 @@
 return {
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
-        }
-    }
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
 }
